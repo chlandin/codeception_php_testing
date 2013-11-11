5e010433592902e0aa66ec688874e3776a62a332
Christian Landin <christian.landin@me.com>
2013-11-11 15:12:54 +0100

    Forgot admin.php

---
+++ admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8" />
+    </head>
+    <body>
+        <?php echo "<h1>Welcome, " . htmlspecialchars($_GET['username']) . "</h1>"; ?>
+    </body>
+</html>
+
+
