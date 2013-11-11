262859cc7a14393d4e50902e3d5aa28890a093ca
Christian Landin <christian.landin@me.com>
2013-11-11 15:11:05 +0100

    Further testing

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
         <meta charset=utf-8" />
     </head>
     <body>
-        <h1>Welcome</h1>
+        <!-- $_GET for testing purposes only -->
+        <form id="testForm" method="get" action="admin.php">
+            <label for="">JohnDoe -> </label>
+            <input type="text" name="username" id="username">
+            <br />
+            <label for="">1234 -> </label>
+            <input type="text" name="password" id="password">
+            <button type="submit">Submit</button>
+        </form>
     </body>
 </html>
 
--- tests/acceptance/HomePageCept.php
+++ tests/acceptance/HomePageCept.php
@@ -3,4 +3,16 @@
 $I = new WebGuy($scenario);
 $I->wantTo('verify that the home page welcomes me');
 $I->amOnPage('/');
-$I->see('Welcome', 'h1');
+
+$I->fillField('username', 'JohnDoe');
+$I->fillField('password', '1234');
+$I->click('Submit');
+
+/* $I->submitForm('#testForm', [ */
+/*     'username' => 'JohnDoe', */
+/*     'password' => '1234', */
+/*     ]); */
+
+/* $I->seeCurrentUrlEquals('/admin.php'); */
+$I->seeInCurrentUrl('/admin.php');
+$I->see('Welcome, JohnDoe', 'h1');
